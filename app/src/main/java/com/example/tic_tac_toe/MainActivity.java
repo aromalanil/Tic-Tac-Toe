4731153010a9fab3b7dac095b253c3caedf8b26d
Aromal Anil <aromalanilkannan@gmail.com>
2019-10-23 17:13:54 +0530

    Optimisation

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.gridlayout.widget.GridLayout;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     //Yellow :1 Red: 0
     int currentPlayer=0;
     // No : 2 Clicked by yellow : 1, clicked by Red :0
-    int[] isClicked = {2,2,2,2,2,2,2,2,2};
+    int[] coinLocation = {2,2,2,2,2,2,2,2,2};
+    //Each element denotes a winning sequence
     int[][] winningPositions ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     boolean won=false;
     boolean isDraw=false;
@@ -30,29 +30,29 @@ public class MainActivity extends AppCompatActivity {
     public void coinClick(View view)
     {
         ImageView coin = (ImageView) view;
-        TextView redScore = (TextView) findViewById(R.id.redScore);
-        TextView yellowScore = (TextView) findViewById(R.id.yellowScore);
+        TextView redScore = findViewById(R.id.redScore);
+        TextView yellowScore = findViewById(R.id.yellowScore);
         int position = Integer.parseInt(coin.getTag().toString());
-        if(isClicked[position]==2&&!won&&!isDraw)
+        if(coinLocation[position]==2&&!won&&!isDraw)
         {
             if (currentPlayer == 1) {
                 coin.setImageResource(R.drawable.yellow_coin);
-                isClicked[position] = currentPlayer;
+                coinLocation[position] = currentPlayer;
                 currentPlayer = 0;
 
             } else if(currentPlayer==0&!won&&!isDraw) {
                 coin.setImageResource(R.drawable.red_coin);
-                isClicked[position] = currentPlayer;
+                coinLocation[position] = currentPlayer;
                 currentPlayer = 1;
             }
             coin.setAlpha(1f);
         }
-        for(int wp[]: winningPositions)
+        for(int[] wp : winningPositions)
         {
-            if(isClicked[wp[0]]==isClicked[wp[1]]&&isClicked[wp[1]]==isClicked[wp[2]]&&isClicked[wp[0]]!=2&&!won)
+            if(coinLocation[wp[0]]== coinLocation[wp[1]]&& coinLocation[wp[1]]== coinLocation[wp[2]]&& coinLocation[wp[0]]!=2&&!won)
             {
                 won=true;
-             if(isClicked[wp[0]]==1)
+             if(coinLocation[wp[0]]==1)
              {
                  showDialog("Yellow Wins!");
                  yellow++;
@@ -67,9 +67,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         isDraw=true;
-        for(int i=0;i<isClicked.length;i++)
+        for(int i = 0; i< coinLocation.length; i++)
         {
-            if(isClicked[i]==2)
+            if(coinLocation[i]==2)
             {
                 isDraw=false;
             }
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
     public void showDialog(String message)
     {
         findViewById(R.id.linearLayout).setVisibility(View.VISIBLE);
-        TextView msg = (TextView) findViewById(R.id.messageText);
+        TextView msg = findViewById(R.id.messageText);
         msg.setText(message);
 
 
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         {
             ImageView image =(ImageView) grid.getChildAt(i);
             image.setAlpha(0f);
-            isClicked[i]=2;
+            coinLocation[i]=2;
 
         }
         isDraw=false;
